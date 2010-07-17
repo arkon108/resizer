24f273b26f7a5097c960f5ad1ebb54b1146e2df2
Saša Tomislav Mataić <sasa.tomislav@mataic.com>
2010-07-17 03:48:59 +0200

    cleaned up html

--- index.php
+++ index.php
@@ -18,29 +18,42 @@ $sid = session_id();
 <div id="wrapper">
 
     <div id="header">
-        <h1>Resize yo' images</h1>
-        <p class="ui-widget-content ui-corner-all">
-        	This is a demo application for batch resizing of images. 
-            The source code is available at 
-            <a href="http://github.com/sasatomislav/resizer">http://github.com/sasatomislav/resizer</a>. 
-       	</p>
-        <p class="ui-widget-content ui-corner-all">Upload up to 5 images and get resized variations in a .zip file</p>
+        <h1>Thumbnailer</h1>
+        <h2 id="tagline" class="ui-state-disabled">resize a bunch of images at once<sup>*</sup></h2>
     </div>
 
     <div id="content">
     
         <div id="step-1" class="content-step">
-            <h2>What sizes do you need to get?</h2>
-            
             <div class="tabs">
-	            <ul>
-	            	<li><a href="#resize">Resize to fixed dimensions</a></li>
-	            	<li><a href="#resizeProportional">Resize to maximum</a></li>
-	            	<li><a href="#cropResize">Resize & crop</a></li>
+	            <ul class="ui-tabs-nav">
+	            	<li><a href="#cropResize">Thumbnail them</a></li>
+	            	<li><a href="#resizeTo">Resize to maximum size</a></li>
+	            	<li><a href="#resize">Resize</a></li>
 	            </ul>
+	            <div id="cropResize">
+	            	<div class="staging ui-widget-content">
+	            		<div class="image-thumbnail-mock ui-widget-header ui-state-error" title="drag my corner to resize me!"></div>
+	            	</div>
+	            	<label for="resize-crop-width">Width <input type="text" value="133" class="input-text-short" name="width" id="resize-crop-width" /> px</label>
+	            	&#215;
+	            	<label for="resize-crop-height">Height <input type="text" value="100" class="input-text-short" name="height" id="resize-crop-height" /> px</label>
+	            	<hr class="ui-state-disabled" />
+	            	<button class="use-this-resize fe-button">Ok</button>
+	            </div>
+	            <div id="resizeTo">
+	            	<p>Set maximum width and height for resized images.</p>
+	            	<p>If image is smaller, no resizing will be done. Resizing is done proportionally (keeping the aspect ratio).</p>
+	            	<hr class="ui-state-disabled" />
+	            	<label for="resize-to-width">Width <input type="text" class="input-text-short" name="width" id="resize-to-width" /> px</label>
+	            	&#215;
+	            	<label for="resize-to-height">Height <input type="text" class="input-text-short" name="height" id="resize-to-height" /> px</label>
+	            	<hr class="ui-state-disabled" />
+	            	<button class="use-this-resize fe-button">Ok</button>
+	            </div>
 	            <div id="resize">
 	            	<p>Define to which width and height the images will be resized.</p>
-	            	<p>Note that images will be distorted if aspect ratio of entered dimensions is different than on original image.</p>
+	            	<p>Note that images could be distorted.</p>
 	            	<hr class="ui-state-disabled" />
 	            	<label for="resize-width">Width <input type="text" class="input-text-short" name="width" id="resize-width" /> px</label>
 	            	&#215;
@@ -48,43 +61,33 @@ $sid = session_id();
 	            	<hr class="ui-state-disabled" />
 	            	<button class="use-this-resize fe-button">Ok</button>
 	            </div>
-	            <div id="resizeProportional">
-	            	<p>Set maximum width or height images should not exceed.</p>
-	            	<p>If image dimensions are within these limits, image will not be resized.</p>
-	            	<p>Note that only one dimension must be set.</p>
-	            	<hr class="ui-state-disabled" />
-	            	<label for="resize-proportional-width">Width <input type="text" class="input-text-short" name="width" id="resize-proportional-width" /> px</label>
-	            	&#215;
-	            	<label for="resize-proportional-height">Height <input type="text" class="input-text-short" name="height" id="resize-proportional-height" /> px</label>
-	            	<hr class="ui-state-disabled" />
-	            	<button class="use-this-resize fe-button">Ok</button>
-	            </div>
-	            <div id="cropResize">
-	            	<p>Resize images to defined dimensions, and crop from center if needed</p>
-	            	<hr class="ui-state-disabled" />
-	            	<label for="resize-crop-width">Width <input type="text" class="input-text-short" name="width" id="resize-crop-width" /> px</label>
-	            	&#215;
-	            	<label for="resize-crop-height">Height <input type="text" class="input-text-short" name="height" id="resize-crop-height" /> px</label>
-	            	<hr class="ui-state-disabled" />
-	            	<button class="use-this-resize fe-button">Ok</button>
-	            </div>
         	</div>
         </div>
         
         <div id="step-2" class="content-step">
-            <h2>Upload</h2>
-            <div class="ui-widget-content ui-corner-all">
+            <div class="intro ui-widget-content ui-corner-all">
             	<p>Select up to 5 files from your computer to upload. File size is limited to 2 MB</p>
             <hr class="ui-state-disabled" />
             <div id="upload-wrapper">
 				<input id="uploadfiles" name="uploadfiles" type="file" />
 			</div>
+			<p id="download-errors" class="ui-state-error ui-corner-all"><span style="float: left; margin-right: 0.3em;" class="ui-icon ui-icon-alert"></span>Some of your images were too big for upload!</p>
 			<a href="#" id="download-zip" class="fe-button button-action">Download resized images</a>
             <a href="#" id="refresh" class="fe-button button-action">Resize again!</a>
             </div>
         </div>
+        <hr class="ui-state-disabled" />
+	<p class="ui-widget-content ui-corner-all">* Actually, you can upload and resize only up to 5 images (each being up to 2MB).</p>
+	
+	<p class="ui-widget-content ui-corner-all">
+        	This is a demo application and a prototype. 
+            The source code is available at 
+            <a href="http://github.com/sasatomislav/resizer">http://github.com/sasatomislav/resizer</a>. 
+	</p>
     </div>
-
+    
+	
+	
 </div>
 <script type="text/javascript">
 	var dirname = '<?php echo md5($sid) ?>';
--- js/default.js
+++ js/default.js
@@ -1,80 +1,96 @@
 var pbid = 0;
-var value = 1;
 var resizedata = new Object();
+
+
 $(document).ready(
-        function() {
-        	
+		function() {      	
         	$('.fe-button').button();
         	$('#download-zip').hide();
+        	$('.image-thumbnail-mock').resizable({
+    			resize: function(e,u) {
+        			$(this).parent().parent().find('input[name=width]').val(u.size.width);
+        			$(this).parent().parent().find('input[name=height]').val(u.size.height);
+        		}
+        	}); 
         	$('#refresh').hide();
+        	$('#resize-crop-width').change( function() { $('#cropResize .image-thumbnail-mock').css('width', $(this).val() + 'px'); } );
+        	$('#resize-crop-height').change( function() { $('#cropResize .image-thumbnail-mock').css('height', $(this).val() + 'px'); } );
         	$('.tabs').tabs();
+        	$('.staging').slideDown();
         	
-        	$('.use-this-resize').click(
-        			function() { 
-        				useResize(this);
-        			}
-        	);
+        	$('.use-this-resize').click( function() { 
+        		if (hasResizeDataErrors(this)) {
+        			alert(hasResizeDataErrors());
+        		} else {
+        			useResize(); 
+        		}
+        	});
 });     	
 
 
-function useResize(buttonElement)
+
+
+function useResize()
 {
+	$('#step-1').fadeOut(200, 
+	        function() { 
+	            $('#step-2').fadeIn(200);
+	            initUploader();
+	        });
+}
+
+function initUploader()
+{
+	$('#uploadfiles').uploadify({
+        uploader    : 'js/jquery.uploadify-v2.1.0/uploadify.swf',
+        script      : 'uploadify.php',
+        cancelImg   : 'js/jquery.uploadify-v2.1.0/cancel.png',
+        auto        : true,
+        folder      : 'uploads/' + dirname + '/',
+        multi       : true,
+        fileDesc    : 'Image files',
+        fileExt     : '*.gif;*.jpg;*.jpeg;*.png;*.GIF;*.JPG;*.JPEG;*.PNG',
+        sizeLimit   : 2097152,
+        buttonText  : 'Select',
+        wmode       : 'transparent',
+        scriptData  : resizedata,
+        onError     : function(e,q,f,err) { /*alert('Uh-oh, your image is over 2MB and cannot be resized :(');*/ },
+        queueSizeLimit : 5,
+        onAllComplete : function(e,d) 
+            { 
+        		$('#step-2 .intro p').fadeOut(300); 
+        		$('#step-2 hr').fadeOut(300); 
+        		if (d.filesUploaded > 0) {
+        			$('#upload-wrapper').fadeOut(); 
+        			$('#download-zip').fadeIn();
+        			if (d.errors) {
+        				$('#download-errors').show();
+        			}
+        			
+        			
+        			$('#download-zip').attr('href', 'download.php?dir=' + dirname + '/' + resizedata.resizeType + '_' + resizedata.width + 'x' + resizedata.height);
+        			$('#download-zip').click(function() { $(this).fadeOut(300, function() { $('#refresh').fadeIn(); $('#refresh').click(function(){ window.location.href = window.location.href; }); }); return true; });        			
+        		}
+        		
+        		
+        		
+            }
+        });
+}
+
+function hasResizeDataErrors(buttonElement)
+{
+	var error = false;
 	resizedata.resizeType = $(buttonElement).parent().attr('id');
 	resizedata.width = parseInt($(buttonElement).parent().find('input[name="width"]').val());
 	resizedata.width = (isNaN(resizedata.width)) ? 0 : resizedata.width ;
 	resizedata.height = parseInt($(buttonElement).parent().find('input[name="height"]').val());
-	resizedata.height = (isNaN(resizedata.height)) ? 0 : resizedata.height ;
-	
+	resizedata.height = (isNaN(resizedata.height)) ? 0 : resizedata.height ;	
 	resizedata.dir = 'uploads' + '/' + dirname;
-	errors = false;
-	errorMsg = '';
-	switch (resizedata.resizeType) {
-	case 'resizeProportional':
-		if (resizedata.width && resizedata.height) {
-			errors = true;
-			errorMsg = "You have to enter only one dimension!\nOnly numbers are accepted";
-		}
-		break;
-	default:
-		if (0 == resizedata.width || 0 == resizedata.height) {
-			errors = true;
-			errorMsg = "You have to enter both width and height!\nOnly numbers are accepted";
-		}
-		break;
+	
+	if (0 == resizedata.width || 0 == resizedata.height) {
+		error = "You have to enter both width and height!\nOnly numbers are accepted";
 	}
 	
-	if (errors) {
-		alert(errorMsg);
-		return false;
-	}
-	
-	$('#step-1').fadeOut(200, 
-	        function() { 
-	            $('#step-2').fadeIn(200);
-	            
-	            $('#uploadfiles').uploadify({
-	                uploader    : 'js/jquery.uploadify-v2.1.0/uploadify.swf',
-	                script      : 'uploadify.php',
-	                cancelImg   : 'js/jquery.uploadify-v2.1.0/cancel.png',
-	                auto        : true,
-	                folder      : 'uploads/' + dirname + '/',
-	                multi       : true,
-	                fileDesc    : 'Image files',
-	                fileExt     : '*.gif;*.jpg;*.jpeg;*.png;*.GIF;*.JPG;*.JPEG;*.PNG',
-	                sizeLimit   : 2097152,
-	                buttonText  : 'Select',
-	                wmode       : 'transparent',
-	                scriptData  : resizedata,
-	                queueSizeLimit : 5,
-	                onAllComplete : function(e,d) 
-	                    { 
-	                        $('#upload-wrapper').fadeOut(); 
-	                        $('#download-zip').fadeIn();
-	                        $('#download-zip').attr('href', 'download.php?dir=' + dirname + '/' + resizedata.resizeType + '_' + resizedata.width + 'x' + resizedata.height);
-	                        $('#download-zip').click(function() { $(this).fadeOut(300, function() { $('#refresh').fadeIn(); $('#refresh').click(function(){ window.location.href = window.location.href; }) }); return true; });
-	                        
-                        }
-	                });
-	        });
-	
+	return error;
 }
